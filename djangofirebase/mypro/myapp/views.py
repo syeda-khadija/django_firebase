6e983875cf54adf17268aa0e3b8e775377046bd6
Syeda_Khadija <130871231+syeda-khadija@users.noreply.github.com>
2025-09-09 12:12:48 +0500

    commit

--- djangofirebase/mypro/myapp/views.py
+++ djangofirebase/mypro/myapp/views.py
@@ -1,3 +1,19 @@
-from django.shortcuts import render
+from django.shortcuts import  render, redirect
+from mypro.firebase_config import db
+from django.contrib import messages
 
-# Create your views here.
+def Contacts{request}:
+    if request.method=="POST":
+        a = request.POST.get("name")
+        b = request.POST.get("email")
+        c = request.POST.get("message")
+
+        db.collection("contact").add({
+            "Name": a,
+            "Email" :b,
+            "Msg": c
+        })
+        messages.success(request,"query has been submitted")
+        return redirect("con")
+
+       return render(request,"myapp/contact.html")
